f6d7ec535d6e5c19d936afc2ef3fa8fddcfb4409
mika <mikaellajarapa@gmail.com>
2021-09-24 23:06:14 +0800

    add root of database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose' 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb://localhost:27017/nestJS-app')],
   controllers: [AppController],
   providers: [AppService],
 })
 
-export class AppModule {}
+export class AppModule { }
