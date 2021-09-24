b11f0f6722af6b3d593b636867f9cdd917608b99
mika <mikaellajarapa@gmail.com>
2021-09-24 23:05:54 +0800

    update product model

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,7 +1,11 @@
-export class Product {
-    constructor(
-        public id: string,
-        public name: string,
-        public price: string,
-    ) { }
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+})
+export interface Product extends mongoose.Document {
+    id: string;
+    name: string;
+    price: number;
 }
